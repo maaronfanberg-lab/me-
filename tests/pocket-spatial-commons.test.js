353c2e53b5b8a001ed20267398d6a2471182fda9
maaronfanberg-lab <maaronfanberg@gmail.com>
2026-09-06 14:52:47 -0500

    Test Commons catalog button click path

--- tests/pocket-spatial-commons.test.js
+++ tests/pocket-spatial-commons.test.js
@@ -64,4 +64,80 @@ assert(url.indexOf('callback=cb')!==-1);
 assert(url.indexOf('client_id')===-1);
 assert(url.indexOf('token')===-1);
 
-console.log('Pocket Spatial Wikimedia Commons transport tests passed.');
+function FakeElement(tag){
+  this.tagName=tag;
+  this.children=[];
+  this.style={};
+  this.parentNode=null;
+  this.textContent='';
+  this.className='';
+  this.listeners={};
+  this.firstChild=null;
+  this.disabled=false;
+}
+FakeElement.prototype.appendChild=function(child){
+  child.parentNode=this;
+  this.children.push(child);
+  this.firstChild=this.children[0]||null;
+  return child;
+};
+FakeElement.prototype.removeChild=function(child){
+  const i=this.children.indexOf(child);
+  if(i>=0)this.children.splice(i,1);
+  this.firstChild=this.children[0]||null;
+  child.parentNode=null;
+};
+FakeElement.prototype.insertBefore=function(child,before){
+  child.parentNode=this;
+  const i=this.children.indexOf(before);
+  if(i<0)this.children.push(child);else this.children.splice(i,0,child);
+  this.firstChild=this.children[0]||null;
+};
+FakeElement.prototype.addEventListener=function(type,handler){this.listeners[type]=handler;};
+FakeElement.prototype.setAttribute=function(name,value){this[name]=value;};
+FakeElement.prototype.click=function(){
+  assert(this.listeners.click,'button missing click listener');
+  this.listeners.click({type:'click',target:this});
+};
+
+const hero=new FakeElement('div');
+const page=new FakeElement('main');
+page.appendChild(hero);
+const head=new FakeElement('head');
+let appendedScript=null;
+head.appendChild=function(child){appendedScript=child;child.parentNode=head;return child;};
+const fakeDocument={
+  querySelector:function(selector){return selector==='.card.hero'?hero:null;},
+  createElement:function(tag){return new FakeElement(tag);},
+  createTextNode:function(text){const n=new FakeElement('#text');n.textContent=text;return n;},
+  head:head
+};
+
+const uiSnippet="var root=this;\nvar API='https://commons.wikimedia.org/w/api.php';\nvar CATEGORY='Category:Audio files of music';\nvar jsonpCounter=0;\nvar state={tracks:[],lastProbe:null};\n"+
+  sliceFunction('el','button')+'\n'+
+  sliceFunction('button','setStatus')+'\n'+
+  sliceFunction('setStatus','removeNode')+'\n'+
+  sliceFunction('removeNode','buildCatalogURL')+'\n'+
+  sliceFunction('buildCatalogURL','jsonp')+'\n'+
+  sliceFunction('jsonp','plain')+'\n'+
+  'function normalizePage(){return null;}\nfunction renderTracks(){}\n'+
+  sliceFunction('loadCatalog','createUI')+'\n'+
+  sliceFunction('createUI','boot')+'\n'+
+  'this.makeUI=createUI;';
+
+const uiSandbox={
+  document:fakeDocument,
+  encodeURIComponent:encodeURIComponent,
+  setTimeout:function(){return 1;},
+  clearTimeout:function(){}
+};
+vm.runInNewContext(uiSnippet,uiSandbox,{filename:'commons-ui-click-extract.js'});
+const ui=uiSandbox.makeUI();
+assert(ui&&ui.load,'Commons UI should create a load button');
+assert.doesNotThrow(function(){ui.load.click();},'Commons catalog button click must not throw');
+assert.strictEqual(ui.load.disabled,true);
+assert.strictEqual(ui.load.textContent,'LOADING COMMONS…');
+assert(ui.status.textContent.indexOf('Requesting a small batch')!==-1);
+assert(appendedScript&&appendedScript.src.indexOf('callback=PocketSpatialCommonsJSONP1')!==-1,'catalog click should append JSONP request script');
+
+console.log('Pocket Spatial Wikimedia Commons transport and catalog-button tests passed.');
