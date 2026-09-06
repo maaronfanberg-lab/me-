dfb913d3a1fb8c3a8aac7a41f63f89d606daa2a3
maaronfanberg-lab <maaronfanberg@gmail.com>
2026-09-06 14:26:37 -0500

    Make Jamendo contract test headless-safe

--- tests/pocket-spatial-jamendo.test.js
+++ tests/pocket-spatial-jamendo.test.js
@@ -3,21 +3,24 @@ const fs=require('fs');
 const vm=require('vm');
 
 const source=fs.readFileSync('apps/pocket-spatial-jamendo.js','utf8');
-const sandbox={
-  console:console,
-  setTimeout:setTimeout,
-  clearTimeout:clearTimeout,
-  setInterval:setInterval,
-  clearInterval:clearInterval,
-  document:{readyState:'loading',addEventListener:function(){},createElement:function(){return {};},head:{appendChild:function(){}}}
-};
-sandbox.window=sandbox;
-sandbox.self=sandbox;
-vm.runInNewContext(source,sandbox,{filename:'pocket-spatial-jamendo.js'});
 
-const api=sandbox.PocketSpatialJamendo;
-assert(api,'PocketSpatialJamendo API missing');
-assert.strictEqual(api.testClientId,'709fa152');
+function sliceFunction(name,nextName){
+  const start=source.indexOf('function '+name+'(');
+  const end=source.indexOf('\nfunction '+nextName+'(',start);
+  assert(start>=0,'missing function '+name);
+  assert(end>start,'missing boundary after '+name);
+  return source.slice(start,end);
+}
+
+const constants="var TEST_CLIENT_ID='709fa152';\nvar API='https://api.jamendo.com/v3.0/tracks/';\n";
+const snippet=constants+
+  sliceFunction('buildTracksURL','jsonp')+'\n'+
+  sliceFunction('licenseURL','licenseLabel')+'\n'+
+  'this.api={buildTracksURL:buildTracksURL,licenseAllowsSpatial:licenseAllowsSpatial};';
+
+const sandbox={encodeURIComponent:encodeURIComponent};
+vm.runInNewContext(snippet,sandbox,{filename:'jamendo-pure-extract.js'});
+const api=sandbox.api;
 
 assert.strictEqual(api.licenseAllowsSpatial({license_ccurl:'https://creativecommons.org/licenses/by/4.0/'}),true);
 assert.strictEqual(api.licenseAllowsSpatial({license_ccurl:'https://creativecommons.org/licenses/by-sa/4.0/'}),true);
